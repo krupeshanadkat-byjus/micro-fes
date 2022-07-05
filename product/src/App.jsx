7f63630e861b19781868099859952b0231e2718f
krupeshanadkat-byjus <krupesh.anadkat@byjus.com>
2022-07-05 17:49:22 +0530

    Made Product Page Dynamic With Route Params

--- product/src/App.jsx
+++ product/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const Header = React.lazy(() => import('home/Header'))
 import Footer from 'home/Footer'
@@ -10,17 +11,21 @@ import ProductContent from './ProductContent'
 
 const App = () => {
 	return (
-		<div className='product-page-wrapper'>
-			<SafeComponent>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Header />
-				</Suspense>
-			</SafeComponent>
-			<div className='text-3xl mx-auto max-w-6xl'>
-				<ProductContent />
+		<Router>
+			<div className='product-page-wrapper'>
+				<SafeComponent>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Header />
+					</Suspense>
+				</SafeComponent>
+				<div className='text-3xl mx-auto max-w-6xl'>
+					<Routes>
+						<Route path='/products/:id' element={<ProductContent />}></Route>
+					</Routes>
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div>
+		</Router>
 	)
 }
 ReactDOM.render(<App />, document.getElementById('app'))
--- product/src/ProductContent.jsx
+++ product/src/ProductContent.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { getProductById, currency } from 'home/products'
 
 const ProductContent = () => {
-	const id = 1
+	const { id } = useParams()
 	const [product, setProduct] = useState({})
 
 	useEffect(() => {
